2aefa880f42c52c9f03bc43b1b96633b1c172667
Antoine <antoine@ewill.fr>
2016-08-07 18:21:25 +0200

    :rocket: Added money system, daily money and games give money

--- BitusEnormus.js
+++ BitusEnormus.js
@@ -8,7 +8,16 @@ var mongoose = require('mongoose'),
 		bot = new Discord.Client({
 		    autorun: true,
 		    token: Utils.token
-		});
+		}),
+		Cleverbot = require('cleverbot-node');
+
+cleverbot = new Cleverbot();
+
+var initRoll = false;
+var hasRolled = false;
+var isFirst = false;
+var playerOne = {};
+var playerTwo = {};
 
 //Change your IP Adress here!
 const IP_ADRESS = Utils.ip;
@@ -22,28 +31,37 @@ if(env === "development"){
 	mongoose.connect('mongodb://'+IP_ADRESS+'/BitusEnormus');
 }
 var ladderboardSchema = new Schema({
-		name: String,
-		victories: Number
+	name: String,
+	victories: {type: Number, default: 0},
+	bank: {type: Number, default: 0},
+	date: Date
+});
+var firstSchema = new Schema({
+  name: String,
+	timesHasBeenFirst: Number
 });
 var ladderboardPlayer = mongoose.model('Ladder', ladderboardSchema);
+var Firster = mongoose.model('First', firstSchema);
 
-
-// var antoine = new ladderboardPlayer({name: 'antoine', victories:54});
-// var romain = new ladderboardPlayer({name: 'romain', victories:34});
-// var remi = new ladderboardPlayer({name: 'remi', victories:34});
-// var pierre = new ladderboardPlayer({name: 'pierre', victories:3});
-// antoine.save();
-// romain.save();
-// remi.save();
-// pierre.save();
 // bot.on('ready', function(event) {});
 // bot.on('presence', function(user, userID, status, gameName, event) {});
 // Save those for later on!
 
+bot.on('ready', function(event){
+	bot.setPresence({
+		game: "coding"
+	});
+});
 
-var initRoll = false;
-var playerOne = {};
-var playerTwo = {};
+bot.on('any', function(event){
+	if(new Date().getHours() >= 0 && new Date().getHours() < 12){
+		isFirst = false;
+	}else{
+		console.log('waiting for 0h00');
+	}
+});
+
+
 bot.on('message', function(user, userID, channelID, message, event) {
 	if(message === "!roll" && !initRoll){
 		initRoll = true;
@@ -55,12 +73,13 @@ bot.on('message', function(user, userID, channelID, message, event) {
 		});
 	}
 
-	if(message === "!roll me" && initRoll && userID !== playerOne.id){
+	if(message === "!roll me" && initRoll && userID !== playerOne.id && !hasRolled){
+		//Prevent from spamming !roll me that cause to add 'null' in database...
+		hasRolled = true;
 		playerTwo = new Player(user, userID, false, undefined);
 		bot.sendMessage({
 			to:channelID,
-			message: `:rocket:  ${playerOne.name} :game_die: ${playerTwo.name}  :rocket:`,
-			typing:true
+			message: `:rocket:  ${playerOne.name} :game_die: ${playerTwo.name}  :rocket:`
 		});
 
 		/**
@@ -70,6 +89,7 @@ bot.on('message', function(user, userID, channelID, message, event) {
 		setTimeout(function(){
 			rollDice(channelID, playerOne.name, playerTwo.name);
 			initRoll = false;
+			hasRolled = false;
 			playerOne = {};
 			playerTwo = {};
 		}, 1000 );
@@ -91,7 +111,7 @@ bot.on('message', function(user, userID, channelID, message, event) {
 						bot.sendMessage({
 							to: channelID,
 							message: ladder.map((player, index) => {
-								return `**${setPlace(index+1)}** ${setMedal(index+1)} ${player.name} : ${player.victories} victoires !\n\n`;
+								return `**${setPlace(index+1)}** ${setMedal(index+1)} ${player.name} : ${player.victories} victoires et possède ${player.bank} kebabs!\n\n`;
 							}).join('')
 						});
 					}else{
@@ -103,6 +123,28 @@ bot.on('message', function(user, userID, channelID, message, event) {
 				}
 		});
 	}
+	if(message === "!ladder first"){
+		Firster.find({})
+			.sort('-timesHasBeenFirst')
+			.exec(function(err, doc){
+				if(!err){
+					var firstLadder = doc;
+					if(firstLadder.length){
+						bot.sendMessage({
+							to: channelID,
+							message: firstLadder.map((person, index) => {
+									return `**${setPlace(index+1)}** ${setMedal(index+1)} ${person.name} : ${person.timesHasBeenFirst} first!\n\n`;
+								}).join('')
+							});
+						}
+					}else{
+					  bot.sendMessage({
+							to: channelID,
+							message: `:rocket:  Aucun joueur n'a encore firsté  :rocket:`
+						});
+					}
+			});
+	}
 	if(message === "!help"){
 		bot.sendMessage({
 			to:channelID,
@@ -110,6 +152,66 @@ bot.on('message', function(user, userID, channelID, message, event) {
 				return `:arrow_right: ${command.commandName} : ${command.explication} \n\n`;
 			}).join('')
 		});
+	}
+
+	if(message.match(/^(<@198532347765850112>)/g)){
+		var msgArr = message.split(' ');
+		msgArr.shift();
+		Cleverbot.prepare(function(){
+			cleverbot.write(msgArr.join(' '), function(res){
+				bot.sendMessage({
+					to:channelID,
+					message: `:robot:  <@${userID}> ${res.message}  :robot:`
+				});
+			});
+		});
+	}
+
+	if(message === "!first" && !isFirst){
+		firstQuery(user);
+		setMoney(user, 5000);
+		bot.sendMessage({
+			to:channelID,
+			message: `${user} à eu le first a+ les noobix`
+		});
+		isFirst = true;
+	}else if(message === "!first" && isFirst){
+		bot.sendMessage({
+			to:channelID,
+			message: `Trop tard grosse :poop:`
+		});
+	}
+
+	if(message === "!argentstp"){
+		ladderboardPlayer.find({name: user})
+			.exec(function(err, doc){
+				if(!err){
+					if(!doc.length){
+						getTodayMoney(user, channelID);
+					}else{
+						var date = doc[0].date;
+						console.log('date in doc '+date);
+						var dateAfterOneDay = new Date(+doc[0].date + 1*24*60*60*1000);
+						console.log('date after 1 day '+dateAfterOneDay);
+						var dateNow = new Date();
+						console.log('date maintenant '+dateNow);
+
+						if(dateNow > dateAfterOneDay){
+							getTodayMoney(user, channelID);
+						}
+						else if(dateNow > date && dateNow < dateAfterOneDay){
+							bot.sendMessage({
+								to:channelID,
+								message: `Impossible, réessaye demain à ${dateAfterOneDay.getHours()}h${dateAfterOneDay.getMinutes()}m${dateAfterOneDay.getSeconds()}s`
+							});
+						}
+					}
+				}
+			});
+	}
+
+	if(message==="issou"){
+		sendFiles(channelID, ['./images/issou.jpg']);
 	}
 });
 
@@ -163,15 +265,19 @@ function rollDice(channelID, initName, adversaireName){
 
 	if(playerOne.result > playerTwo.result){
 		rollQuery(playerOne.name);
+		setMoney(playerOne.name, 100);
+		setMoney(playerTwo.name, -50);
 		return bot.sendMessage({
 			to: channelID,
-			message: `:rocket:  ${initName} : **${playerOne.result}** | ${adversaireName} : **${playerTwo.result}**, **${initName} wonnered!**  :rocket:`
+			message: `:rocket:  ${initName} : **${playerOne.result}** | ${adversaireName} : **${playerTwo.result}**, **${initName} wonnered! +100 kebabs**  :rocket:`
 		});
 	}else{
 		rollQuery(playerTwo.name);
+		setMoney(playerTwo.name, 100);
+		setMoney(playerOne.name, -50);
 		return bot.sendMessage({
 			to: channelID,
-			message: `:rocket:  ${adversaireName} : **${playerTwo.result}** | ${initName} : **${playerOne.result}**, **${adversaireName} wonnered!**  :rocket:`
+			message: `:rocket:  ${adversaireName} : **${playerTwo.result}** | ${initName} : **${playerOne.result}**, **${adversaireName} wonnered! +100 kebabs**  :rocket:`
 		});
 	}
 }
@@ -195,13 +301,44 @@ function getRandomNumber(){
  */
 function rollQuery(playerName){
 	var query = {name: playerName};
-	var update = {$inc: {victories:1} };
+	var update = {$inc: {'victories':1} };
 	var options = {upsert: true, new: true, setDefaultsOnInsert: true};
 	ladderboardPlayer.findOneAndUpdate(query, update, options, function(err, doc){
 		if(!err) console.error('updated!');
 	});
 }
 
+function firstQuery(firsterName){
+	var query = {name: firsterName};
+	var update = {$inc: {'timesHasBeenFirst':1} };
+	var options = {upsert: true, new: true, setDefaultsOnInsert: true};
+	Firster.findOneAndUpdate(query, update, options, function(err, doc){
+		if(!err) console.error('updated!');
+	});
+}
+
+function setMoney(playerName, amount){
+	var query = {name: playerName};
+	var update = {$inc: {'bank': amount} };
+	var options = {upsert: true, new: true, setDefaultsOnInsert: true};
+	ladderboardPlayer.findOneAndUpdate(query, update, options, function(err, doc){
+		if(!err) console.error('updated!');
+	});
+}
+function getTodayMoney(user, channelID){
+	var query = {name: user};
+	var update = {$inc: {'bank':500} , $set: {date: new Date()}};
+	var options = {upsert: true, new: true, setDefaultsOnInsert: true};
+	ladderboardPlayer.findOneAndUpdate(query, update, options, function(err, doc){
+		if(err) console.log(err);
+		if(!err) console.error('Gave 500 kebabs to '+user);
+	});
+
+	bot.sendMessage({
+		to:channelID,
+		message: `+500 kebabs pour ${user}`
+	});
+}
 
 /**
  * Player - Player constructor
